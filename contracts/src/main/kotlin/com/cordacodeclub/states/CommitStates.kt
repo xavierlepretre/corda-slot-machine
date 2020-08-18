b106f7cbc07bd7f4f7b2fd5e822ce6701abfc90f
Xavier Leprêtre <xavierlepretre@users.noreply.github.com>
2020-08-18 18:19:31 +0400

    Bug fix in commit hash calculation.

--- contracts/src/main/kotlin/com/cordacodeclub/states/CommitStates.kt
+++ contracts/src/main/kotlin/com/cordacodeclub/states/CommitStates.kt
@@ -3,21 +3,22 @@ package com.cordacodeclub.states
 import net.corda.core.contracts.ContractState
 import net.corda.core.crypto.SecureHash
 import net.corda.core.identity.AbstractParty
+import net.corda.core.serialization.CordaSerializable
 
 data class CommitState(val hash: SecureHash, val creator: AbstractParty) : ContractState {
     override val participants: List<AbstractParty> = listOf(creator)
 }
 
+@CordaSerializable
 data class CommitImage(val picked: Long, val salt: Long) {
-    val hash: SecureHash
-
-    init {Long.MAX_VALUE
-
-        hash = SecureHash.sha256(ByteArray(16) { index ->
-            if (index == 0) (picked and 0xFF).toByte()
-            else if (index < 8) ((picked ushr (index * 8)) and 0xFF).toByte()
-            else if (index == 9) (salt and 0xFF).toByte()
-            else ((salt ushr ((index - 8) * 8)) and 0xFF).toByte()
+    val hash: SecureHash by lazy {
+        SecureHash.sha256(ByteArray(16) { index ->
+            (when {
+                index == 0 -> picked and 0xFF
+                index < 8 -> picked ushr (index * 8) and 0xFF
+                index == 8 -> salt and 0xFF
+                else -> salt ushr ((index - 8) * 8) and 0xFF
+            }).toByte()
         })
     }
 }
--- contracts/src/test/kotlin/com/cordacodeclub/states/CommitStatesTests.kt
+++ contracts/src/test/kotlin/com/cordacodeclub/states/CommitStatesTests.kt
@@ -9,7 +9,16 @@ class CommitStatesTests {
     private val ledgerServices = MockServices()
 
     @Test
-    fun `commit image hash is correct`() {
-        assertEquals(SecureHash.zeroHash, CommitImage(5003932113484729189L, 6320170081553289949L).hash)
+    fun `all bits are in the hash`() {
+        val leftMostBit = 0x4000000000000000L
+        val allHashes = mutableSetOf<SecureHash>()
+
+        for (i in 0..63) {
+            allHashes.add(CommitImage(0L, leftMostBit shr i).hash)
+            allHashes.add(CommitImage(leftMostBit shr i, 0L).hash)
+        }
+
+        // The 0, 0 was duplicated.
+        assertEquals(127, allHashes.size)
     }
 }
