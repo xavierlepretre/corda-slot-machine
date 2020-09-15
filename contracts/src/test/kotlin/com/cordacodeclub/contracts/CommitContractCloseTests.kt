053f1a817ba18dc43b0a59e26757f3040ca49050
dimashh <dimash97@hotmail.co.uk>
2020-09-15 22:02:11 +0100

    Passing commit contract close test.

--- contracts/src/test/kotlin/com/cordacodeclub/contracts/CommitContractCloseTests.kt
+++ contracts/src/test/kotlin/com/cordacodeclub/contracts/CommitContractCloseTests.kt
@@ -6,9 +6,14 @@ import net.corda.core.crypto.SecureHash
 import net.corda.core.identity.CordaX500Name
 import net.corda.testing.common.internal.testNetworkParameters
 import net.corda.testing.core.TestIdentity
+import net.corda.testing.dsl.LedgerDSL
+import net.corda.testing.dsl.TestLedgerDSLInterpreter
+import net.corda.testing.dsl.TestTransactionDSLInterpreter
 import net.corda.testing.node.MockServices
-import net.corda.testing.node.transaction
+import net.corda.testing.node.ledger
 import org.junit.Test
+import java.math.BigInteger
+import java.time.Instant
 import java.util.*
 
 class CommitContractCloseTests {
@@ -26,32 +31,50 @@ class CommitContractCloseTests {
     private val player = playerId.identity.party
     private val random = Random()
 
+    private fun LedgerDSL<TestTransactionDSLInterpreter, TestLedgerDSLInterpreter>.issueTwoCommits(
+            casinoHash: SecureHash, playerHash: SecureHash) = transaction {
+        val casinoId = UniqueIdentifier()
+        val playerId = UniqueIdentifier()
+        val revealDeadline = Instant.now().plusSeconds(60)
+        input(LockableTokenContract.id, LockableTokenState(casino, issuer, Amount(398L, LockableTokenType)))
+        input(LockableTokenContract.id, LockableTokenState(player, issuer, Amount(3L, LockableTokenType)))
+        output(CommitContract.id, CommittedState(casinoHash, casino,
+                revealDeadline, 2, casinoId))
+        output(CommitContract.id, CommittedState(playerHash, player,
+                revealDeadline, 2, playerId))
+        output(GameContract.id, 3, GameState(casino commitsTo casinoId with (398L issuedBy issuer),
+                player commitsTo playerId with (2L issuedBy issuer), 3,
+                UniqueIdentifier(), listOf(casino, player)))
+        output(LockableTokenContract.id, 2, LockableTokenState(issuer, Amount(400L, LockableTokenType),
+                listOf(casino, player)))
+        output(LockableTokenContract.id, LockableTokenState(player, issuer, Amount(1L, LockableTokenType)))
+        command(casino.owningKey, CommitContract.Commands.Commit(0))
+        command(player.owningKey, CommitContract.Commands.Commit(1))
+        command(listOf(casino.owningKey, player.owningKey), GameContract.Commands.Create(2))
+        command(listOf(casino.owningKey, player.owningKey),
+                LockableTokenContract.Commands.Lock(listOf(0, 1), listOf(3, 4)))
+        verifies()
+    }
+
     @Test
     fun `Close command passes contract`() {
-        ledgerServices.transaction {
-            val casinoId = UniqueIdentifier()
-            val playerId = UniqueIdentifier()
-            val gameStateAndRef = StateAndRef(
-                    TransactionState(
-                            GameState(
-                                    casino commitsTo casinoId with (199L issuedBy issuer),
-                                    player commitsTo playerId with (1L issuedBy issuer),
-                                    3,
-                                    UniqueIdentifier(),
-                                    listOf(casino)),
-                            GameContract.id,
-                            notaryId.party),
-                    StateRef(SecureHash.zeroHash, 1))
-            input(GameContract.id, gameStateAndRef.state.data)
-            input(CommitContract.id, RevealedState(
-                    CommitImage.createRandom(random),
-                    casino,
-                    StaticPointer(gameStateAndRef.ref, GameState::class.java),
-                    UniqueIdentifier(),
-                    listOf(casino)))
-            command(casino.owningKey, CommitContract.Commands.Close(0))
-            command(casino.owningKey, GameContract.Commands.Close)
-            verifies()
+        val casinoImage = CommitImage(BigInteger.valueOf(11L))
+        val playerImage = CommitImage(BigInteger.valueOf(12L))
+        ledgerServices.ledger {
+            val issueTx = issueTwoCommits(casinoImage.hash, playerImage.hash)
+            val (casinoRef, playerRef) = issueTx.outRefsOfType<CommittedState>()
+            val (lockedRef) = issueTx.outRefsOfType<LockableTokenState>()
+            transaction {
+                input(casinoRef.ref)
+                input(playerRef.ref)
+                reference(playerRef.getGamePointer().pointer)
+                timeWindow(TimeWindow.untilOnly(casinoRef.state.data.revealDeadline))
+
+                command(player.owningKey, CommitContract.Commands.Close(0))
+                command(player.owningKey, CommitContract.Commands.Close(1))
+                command(listOf(casino.owningKey, player.owningKey), GameContract.Commands.Close(2))
+                verifies()
+            }
         }
     }
 }
